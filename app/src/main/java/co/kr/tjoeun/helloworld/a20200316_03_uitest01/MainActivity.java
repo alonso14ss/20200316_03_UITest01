91b9f072cd03f52eff899dbac6e2162a04a9497e
alonso14ss <reiyan92@gmail.com>
2020-03-17 11:14:04 +0900

    완성

---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200316_03_uitest01/MainActivity.java
@@ -0,0 +1,14 @@
+package co.kr.tjoeun.helloworld.a20200316_03_uitest01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200316_03_uitest01/SignUpActivity.java
@@ -0,0 +1,14 @@
+package co.kr.tjoeun.helloworld.a20200316_03_uitest01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
